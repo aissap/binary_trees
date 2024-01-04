4d5da94cca0c553801fc4725146f679b620208b2
aissap <jesus.jesus.core@gmail.com>
2024-01-04 11:14:29 -0500

    task-0

---
+++ binary_trees.h
@@ -0,0 +1,20 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Data structure for binary tree node */
+typedef struct binary_tree_s {
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Function prototype for binary_tree_node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif 
